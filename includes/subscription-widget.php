bc0959235bab56b9a53857dc3e5f4953c43346f8
cherryontopio <ivetchadim@gmail.com>
2025-06-09 17:00:03 +0100

    Update subscription-widget

--- includes/subscription-widget.php
+++ includes/subscription-widget.php
@@ -3,7 +3,7 @@
         <h2 class="subs-text">Want to stay in the loop?</h2>
         <div class="logo-name" style="display: none;">
             <div class="subs-logo">
-                <img class="subs-img logo" src="/assets/images/favicon.WebP" alt="Cherry on Top Logo">
+                <img class="subs-img logo" src="/assets/images/favicon.webp" alt="Cherry on Top Logo">
             </div>
             <div class="subs-name">
                 <span class="subs-brand-name">on top .io</span>
